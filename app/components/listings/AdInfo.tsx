0425294c4116faae4717b093961f61259cffa2a4
JackSlice2 <jackslice9@gmail.com>
2024-04-02 19:07:49 -0300

    WhatsApp and Telegram buttons styled

--- app/components/listings/AdInfo.tsx
+++ app/components/listings/AdInfo.tsx
@@ -73,8 +73,18 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
     <div className='col-span-4 flex flex-col gap-8'>
       <div className="flex flex-col gap-2">
         <div className="text-xl font-semibold text-center px-10 justify-between flex flex-row items-center gap-2">
-            <div>Chame no whatApp: <br/> <FaWhatsapp/> </div>
-            <div>Chame no Telegram: <br/> <FaTelegram /> </div>
+        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <div className='bg-green-400 hover:bg-green-600 hover:text-white hover:font-semibold cursor-pointer rounded-2xl text-center p-2 items-center flex gap-1 border-gray-200 border-2 shadow-md' >
+              Chame no whatsApp!
+              <FaWhatsapp/> 
+            </div>
+        </a>
+        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+            <div className='bg-blue-400 hover:bg-blue-600 hover:text-white hover:font-semibold cursor-pointer rounded-2xl text-center p-2 items-center flex gap-1 border-gray-200 border-2 shadow-md' >
+              Chame no Telegram!
+              <FaTelegram /> 
+            </div>
+        </a>
           </div>
           <hr />
             <div className='flex flex-row overflow-x-auto hide-scrollbar'>
@@ -85,10 +95,10 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
                   Horario: <br/>{hours}
               </div>
               <div className='p-3 text-center hover:bg-sky-100 cursor-pointer'>
-                  WhatsApp: <br/>555555555
+                  WhatsApp: <br/>51 985647950
               </div>
               <div className='p-3 text-center hover:bg-sky-100 cursor-pointer'>
-                  Telefone: <br/>555555555
+                  Telefone: <br/>51 985647988
               </div>
               <div className='p-3 text-center hover:bg-sky-100 cursor-pointer'>
                   Fixo: <br/>3434-1422
