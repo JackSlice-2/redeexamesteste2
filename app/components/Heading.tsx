cc78fb73c7f60c542ac5d1e56d05ecbadcd8ef7d
JackSlice2 <jackslice9@gmail.com>
2024-07-06 22:29:48 -0300

    Listing Head Styles

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -15,14 +15,13 @@ const Heading: React.FC<HeadingProps> = ({
     center
 }) => {
   return (
-    <div className={center ? 'text-center' : 'text-start'}>
-        <div className="text-2xl font-bold">
-            {title}
-        </div>
-        <div className="font-light text-neutral-500 mt-2">
-            {subtitle}
-        </div>
-      
+    <div className={`text-center ${center ? '' : 'text-start'} overflow-hidden`}>
+      <div className="text-2xl font-bold overflow-wrap break-word whitespace-normal">
+          {title}
+      </div>
+      <div className="font-light text-neutral-500 mt-2 overflow-wrap break-word whitespace-normal">
+          {subtitle}
+      </div>
     </div>
   )
 }
